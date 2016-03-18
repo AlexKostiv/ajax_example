cd39a6f20b3cac6efbc286c881c9417081b06706
Artur <arturkovalua@gmail.com>
2016-03-18 17:23:07 +0300

    added chenges in products and index

--- public/products.js
+++ public/products.js
@@ -1,133 +1,50 @@
-var AJAX = {
-    GET: function (url, callback) {
-        var xhr = new XMLHttpRequest();
-
-        xhr.open('GET', url, true);
-
-        xhr.send();
-
-        xhr.onreadystatechange = function () {
-            if (this.readyState != 4) return;
-
-            // по окончании запроса доступны:
-            // status, statusText
-            // responseText, responseXML (при content-type: text/xml)
-
-            if (this.status != 200) {
-                // обработать ошибку
-                console.log('Error');
-                return;
-            }
-
-            console.log(xhr.response);
-            callback(JSON.parse(xhr.response));
-            // получить результат из this.responseText или this.responseXML
-        }
-    },
-    POST: function (url, callback, data) {
-        var xhr = new XMLHttpRequest();
-
-        xhr.open('POST', url, true);
-
-        xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+(function () {
+    function List (root){
+        var conteiner = $('.container'),
+            output = conteiner.find('.output'),
+            items = ['hello',10,20,50000],
+            input = conteiner.find('.fieldProduct'),
+            addBtn = conteiner.find('.btn_add');
 
 
-        xhr.send(JSON.stringify(data));
 
-        xhr.onreadystatechange = function () {
-            if (this.readyState != 4) return;
+        function addItem () {
+                addBtn.on('click', function (e) {
+                    items.push(input.val());
+                    render();
+                });
 
-            // по окончании запроса доступны:
-            // status, statusText
-            // responseText, responseXML (при content-type: text/xml)
-
-            if (this.status != 200) {
-                // обработать ошибку
-                console.log('Error');
-                return;
-            }
-
-            console.log(xhr.response);
-            callback(JSON.parse(xhr.response));
-            // получить результат из this.responseText или this.responseXML
         };
-    }
-};
 
-Object.freeze(AJAX);
+        function fetchData () {
+            var xhr = new XMLHttpRequest();
 
-var Products = (function () {
-    function Constructor(root) {
-        this.items = null;
-        this.root = document.querySelector(root);
-        this.listItem = this.root.querySelector('.listProducts');
-        this.addBtn = this.root.querySelector('.add');
-        this.removeBtn = this.root.querySelector('.remove');
-        this.productField = this.root.querySelector('.fieldProduct');
-        this.getItems();
-        this.addEvents();
-    }
+            xhr.open('GET', 'fruites',false);
 
-    Constructor.prototype.render = function () {
-        var self = this;
-        this.listItem.innerHTML = '';
-        this.items.forEach(function (item, i) {
-            var li = document.createElement('li');
-            li.innerHTML = item;
-            self.listItem.appendChild(li);
-            li.addEventListener('click', function () {
-                self.removeFruits(i);
-            })
-        })
-    }
+            xhr.send();
+            if(xhr.status != 200) {
+                console.log(xhr.responseText);
+            }else{
+                items = JSON.parse(xhr.response);
+            };
+        };
 
 
-    Constructor.prototype.getItems = function () {
-        var self = this;
+        function render () {
+            output.html(' ');
+            items.forEach(function (item){
+                var el = $('<li>',{
+                    class: 'newLi',
+                    text: item
+                });
+            output.append(el);
+            });
+        };
 
-        AJAX.GET('fruites', function (data) {
-            self.items = data;
-            self.render();
-        });
+
+        fetchData();
+        addItem();
+        render();
     };
-
-    Constructor.prototype.addEvents = function () {
-        var self = this;
-        this.addBtn.addEventListener('click', function () {
-            self.addFruits();
-        });
-    }
-
-    Constructor.prototype.addFruits = function () {
-        var newItem = {
-                'fruite': this.productField.value
-            },
-            self = this;
-
-        AJAX.POST('fruites', function (data) {
-            self.items = data;
-            self.render();
-        }, newItem);
-    };
-
-    Constructor.prototype.removeFruits = function (id) {
-        var xhr = new XMLHttpRequest();
-
-
-        xhr.open('DELETE', 'fruites/' + id, false);
-
-        xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-
-        xhr.send();
-
-        if (xhr.status != 200) {
-            console.log(xhr.status + ':' + xhr.statusText);
-        } else {
-            console.log(xhr.response);
-            this.items = JSON.parse(xhr.response);
-            this.render();
-        }
-    };
-
-    return Constructor;
-})();
+    var product = new List();
+}())
