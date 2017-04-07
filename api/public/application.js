da9e1c0b5da7d614597061afa718ed85a36b82d9
Ilya Litvinov <ilyaillych@gmail.com>
2017-04-07 14:41:08 +0300

    Added valid webpack config

---
+++ api/public/application.js
@@ -0,0 +1,1 @@
+webpackJsonp([0],{27:function(n,c,e){"use strict";var o=e(1);!function(n){n&&n.__esModule}(o)}},[27]);
---
+++ client../api/public/application.js
@@ -0,0 +1,25 @@
+webpackJsonp([0],{
+
+/***/ 32:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _react = __webpack_require__(5);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Test = function Test() {
+    _classCallCheck(this, Test);
+
+    console.log('Hello wolrd!');
+};
+
+/***/ })
+
+},[32]);
